7e5bbd19ebcd7cc4a0281782a29fcf759461218c
strog <mvr@test.test>
2026-02-21 15:16:17 +0100

    feat: support repo.* calls and more auth ops in impl_steps

--- compiler/emitter/service_impl_steps.go
+++ compiler/emitter/service_impl_steps.go
@@ -110,11 +110,45 @@ func renderImplSteps(steps []normalizer.ImplStep, serviceName, methodName string
 				token := valString(st.CallArgsMap, "refreshToken", "req.RefreshToken")
 				b.WriteString("if s.refreshStore != nil { _ = s.refreshStore.Delete(ctx, " + token + ") }\n")
 			default:
-				b.WriteString(fmt.Sprintf("// TODO impl_steps call %s not supported yet\n", st.CallTarget))
-				b.WriteString("if os.Getenv(\"APP_ENV\") != \"production\" { panic(\"impl_steps call not supported\") }\n")
+				if strings.HasPrefix(strings.ToLower(st.CallTarget), "repo.") {
+					parts := strings.Split(st.CallTarget, ".")
+					if len(parts) >= 3 {
+						repoName := exportName(parts[1])
+						method := strings.ToLower(parts[2])
+						repoVar := fmt.Sprintf("s.%sRepo", repoName)
+						b.WriteString(fmt.Sprintf("if %s == nil { return resp, errors.New(http.StatusInternalServerError, \"Repo error\", \"%s not configured\") }\n", repoVar, repoVar))
+						switch method {
+						case "save":
+							inp := valString(st.CallArgsMap, "input", "nil")
+							b.WriteString(fmt.Sprintf("if err := %s.Save(ctx, %s); err != nil { return resp, err }\n", repoVar, inp))
+						case "findbyid", "get":
+							arg := valString(st.CallArgsMap, "id", "\"\"")
+							b.WriteString(fmt.Sprintf("%s, err := %s.FindByID(ctx, %s)\n", into, repoVar, arg))
+							b.WriteString("if err != nil { return resp, err }\n")
+						case "getbytoken":
+							arg := valString(st.CallArgsMap, "token", "req.RefreshToken")
+							b.WriteString(fmt.Sprintf("%s, err := %s.GetByToken(ctx, %s)\n", into, repoVar, arg))
+							b.WriteString("if err != nil { return resp, err }\n")
+						case "delete":
+							arg := valString(st.CallArgsMap, "id", "\"\"")
+							b.WriteString(fmt.Sprintf("if err := %s.Delete(ctx, %s); err != nil { return resp, err }\n", repoVar, arg))
+						default:
+							b.WriteString(fmt.Sprintf("// TODO impl_steps repo.%s not supported yet\n", method))
+							b.WriteString("if os.Getenv(\"APP_ENV\") != \"production\" { panic(\"impl_steps repo call not supported\") }\n")
+						}
+					} else {
+						b.WriteString("// malformed repo call\n")
+					}
+				} else {
+					b.WriteString(fmt.Sprintf("// TODO impl_steps call %s not supported yet\n", st.CallTarget))
+					b.WriteString("if os.Getenv(\"APP_ENV\") != \"production\" { panic(\"impl_steps call not supported\") }\n")
+				}
 			}
 		case "emit":
-			b.WriteString(fmt.Sprintf("// emit %s not implemented yet\n", st.EmitEvent))
+			b.WriteString(fmt.Sprintf("// emit %s not wired yet\n", st.EmitEvent))
+			if st.EmitExpr != "" {
+				b.WriteString("// payload: " + st.EmitExpr + "\n")
+			}
 			b.WriteString("if os.Getenv(\"APP_ENV\") != \"production\" { panic(\"impl_steps emit not supported\") }\n")
 		default:
 			b.WriteString("// unknown impl_step kind\n")
