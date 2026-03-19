65c625eb1bca2b0b08a24a23a909efd836a80cd7
strog <mvr@test.test>
2026-03-19 22:49:59 +0100

    chore: sync generated artifacts with compiler 0.1.127

---
+++ python/sdk/python/ang_sdk/__init__.py
@@ -0,0 +1,5 @@
+from .client import AngClient, AsyncAngClient
+from .errors import AngAPIError, ProblemDetails
+from . import models
+
+__all__ = ["AngClient", "AsyncAngClient", "AngAPIError", "ProblemDetails", "models"]
